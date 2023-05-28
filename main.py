e5d25f7e840366bb2935833d0ef0f55dfb3cc5dd
Sark <sark.rui@gmail.com>
2023-05-28 15:04:45 +0800

    clean up && add validation

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import requests
 from geoip2.database import Reader
+import os
+import subprocess
+
 
 # Path to GeoLite2-Country.mmdb database file
 path_to_db = './GeoLite2-Country.mmdb'
@@ -33,15 +36,30 @@ def filter_by_country(proxies):
     return filtered_proxies
 
 def save_proxies(filtered_proxies):
+    socks5_dir = 'socks5'
+    os.makedirs(socks5_dir, exist_ok=True)
+
     for country_code, proxies in filtered_proxies.items():
-        with open(f'socks5_{country_code.lower()}.txt', 'w') as f:
+        with open(f'{socks5_dir}/socks5_{country_code.lower()}.txt', 'w') as f:
             for proxy in proxies:
                 f.write(proxy + '\n')
+
+def check_proxies(filtered_proxies):
+    valid_dir = 'valid'
+    os.makedirs(valid_dir, exist_ok=True)
+
+    for country_code in filtered_proxies.keys():
+        filename = f'socks5/socks5_{country_code.lower()}.txt'
+        output_filename = f'{valid_dir}/valid_socks5_{country_code.lower()}.txt'
+        
+        subprocess.run(['./proxy-check', '-r', '-m', '30', '--socks5', '-o', output_filename, '-g', filename])
+
 
 def main():
     proxies = get_socks5_proxies()
     filtered_proxies = filter_by_country(proxies)
     save_proxies(filtered_proxies)
-
+    check_proxies(filtered_proxies)
+    
 if __name__ == '__main__':
     main()
